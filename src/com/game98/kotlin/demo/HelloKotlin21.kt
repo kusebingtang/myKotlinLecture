6ed8ab8e0ed2094f0f261c8b48243272a7ee9b09
jbw <jb@98game.cn>
2019-11-13 09:52:08 +0800

    lateinit 延迟初始化说明

--- src/com/game98/kotlin/demo/HelloKotlin21.kt
+++ src/com/game98/kotlin/demo/HelloKotlin21.kt
@@ -8,8 +8,9 @@ package com.game98.kotlin.demo
  * 有时，这种要求不太方便，比如可以通过依赖注入或单元测试情况下进行赋值
  *
  * 通过lateinit关键字标示属于属性为延迟初始化，需要满足如下3个条件
- *1、lateinit只能用在类体申明的var属性上，不能用在primary constructor
- *
+ *1、，不能用在primary constructor
+ *2、属性不能有自定义的setter 和 getter 方法
+ *3、属性类型非空lateinit只能用在类体申明的var属性上并且不能是原生数据类型
  */
 class TheClass {
 
