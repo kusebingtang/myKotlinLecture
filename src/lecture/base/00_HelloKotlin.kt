99b9fa8946ff3f28b7871220bd45599daf67e079
jbw <jb@98game.cn>
2020-08-26 12:07:26 +0800

    kotlin 加解密

---
+++ src/lecture/base/00_HelloKotlin.kt
@@ -0,0 +1,9 @@
+package lecture.base
+
+/**
+@author JiangBin
+@create 2020-08-21 11:25 */
+
+fun main() {
+    println("hello world!");
+}
---
+++ src/lecture/base/01_main函数.kt
@@ -0,0 +1,9 @@
+package lecture.base
+
+/**
+@author JiangBin
+@create 2020-08-21 11:30 */
+
+fun main(args: Array<String>) {
+
+}
